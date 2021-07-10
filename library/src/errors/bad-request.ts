b7e061441904c5b797036f6a4ee90ee96b3be388
Gustaf Dahl <gustaf-dahl@telia.com>
2021-07-10 14:39:39 +0200

    did a lot of work on library

---
+++ library/src/errors/bad-request.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+import { CustomError } from './custom-error';
+
+export class BadRequestError extends CustomError {
+  statusCode = 400;
+
+  constructor(public message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
+}
---
+++ library/src/misc/secret.ts
@@ -0,0 +1,38 @@
+/** @format */
+
+import fs from 'fs';
+import mkdirp from 'mkdirp';
+import { randomBytes } from 'crypto';
+import dotenv from 'dotenv';
+import { dirname as getDirname } from 'path';
+
+export class Secrets {
+  static path = `${__dirname}/secrets`;
+
+  /*
+    Generate secret key
+    Example use case could be a secret for jsonwebtoken
+    @param {string} name of file and name of key to to be saved into process.env
+  */
+  static async generateKeySecret(name: string) {
+    const secret = randomBytes(32).toString('base64');
+
+    name = name.toUpperCase();
+
+    try {
+      await mkdirp(getDirname(`${this.path}/${name}.env`));
+      fs.writeFileSync(`${this.path}/${name}.env`, `${name}=${secret}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  /*
+    Load secret into process.env
+    @param {string} name of file to load
+  */
+  static async loadSecret(name: string) {
+    name = name.toUpperCase();
+    dotenv.config({ path: `${this.path}/${name}.env` });
+  }
+}
