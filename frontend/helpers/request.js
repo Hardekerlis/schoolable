35bf1e751b0218c6549d84f11e08accce4be92ed
ole1288 <elo12881288@gmail.com>
2021-08-18 01:22:29 +0200

    bug hunting

---
+++ frontend/helpers/request.js
@@ -0,0 +1,74 @@
+class Request {
+
+  constructor(url, body) {
+
+    this.url = 'http://localhost:3001' + url;
+    this.body = body;
+
+    this._json = false;
+
+    this.contentType = 'text/html';
+    this.method = 'POST';
+
+  }
+
+  post() {
+    this.method = 'POST';
+    return this;
+  }
+
+  get() {
+    this.method = 'GET';
+    return this;
+  }
+
+  put() {
+    this.method = 'PUT';
+    return this;
+  }
+
+  delete() {
+    this.method = 'DELETE';
+    return this;
+  }
+
+  patch() {
+    this.method = 'PATCH';
+    return this;
+  }
+
+  json() {
+    this.body = JSON.stringify(this.body);
+    this._json = true;
+    this.contentType = 'application/json';
+    return this;
+  }
+
+  async send() {
+
+    let response;
+
+    const res = await fetch(this.url, {
+      method: this.method,
+      body: this.body,
+      headers: {
+        'Content-Type': this.contentType,
+      },
+      credentials: "include"
+    })
+
+    response = res;
+
+    if(this._json) {
+      response = await res.json();
+      response._response = res;
+    }
+
+    return response;
+
+  }
+
+
+}
+
+export default Request;
