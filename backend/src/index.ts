3b3004fe0e713d79aa8e5878942d65da49c43dd9
Gustaf Dahl <gustaf-dahl@telia.com>
2021-08-23 11:14:41 +0200

    added Schoolable text on startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,51 @@ import { app } from './app';
 import { CONFIG } from './library';
 import { logger } from './logger/logger';
 import { connect } from './database/connect';
+import { textSync } from 'figlet';
+
+const drawSchoolableLogo = async () => {
+  // let rows = [''];
+  //
+  // let terminalRows = 14;
+  // let row = 0;
+  //
+  // for (let i = 0; i < terminalRows + 1; i++) {
+  //   rows[i] = '';
+  // }
+  //
+  // const centerRowNum = Math.round(rows.length / 2 - 1);
+  // const displayCenter = Math.round(rows[centerRowNum].length / 2);
+  //
+  // for (let j = 0; j <= terminalRows; j++) {
+  //   for (let i = 0; i < process.stdout.columns; i++) {
+  //     if (
+  //       i === 0 ||
+  //       i === process.stdout.columns - 1 ||
+  //       row === 0 ||
+  //       row === terminalRows
+  //     ) {
+  //       rows[row] += '#';
+  //       if (i === process.stdout.columns - 1) row++;
+  //     } else rows[row] += ' ';
+  //   }
+  // }
+  //
+  // for (const row of rows) {
+  //   console.log(row);
+  // }
+  // for (const i of rows) {
+  //   console.log(i[0]);
+  // }
+  // console.log(centerRowNum);
+  return textSync('Schoolable platform');
+};
 
 const startServer = async () => {
   const { env } = process;
   console.clear();
+
+  console.log(await drawSchoolableLogo());
+
   logger.info('Starting server...');
 
   if (CONFIG.dev) {
