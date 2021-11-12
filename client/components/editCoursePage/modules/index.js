e4bd48501e9694e7e53b3ee9ec638d36badc60a3
Ole Sund <elo12881288@gmail.com>
2021-11-12 17:46:33 +0100

    started work on making modules and phases drag and dropable in course edit

--- client/components/editCoursePage/modules/index.js
+++ client/components/editCoursePage/modules/index.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
+import { nanoid } from 'nanoid';
+
 import language from 'helpers/lang';
 const lang = language.coursePageEdit.modules;
 
+import { ReactSortable, Sortable, MultiDrag, Swap } from "react-sortablejs";
+
+
+
+
+
 import {
-  PlusClipboard
+  PlusClipboard,
+  Drag,
+  RightArrow
 } from 'helpers/systemIcons'
 
 import {
@@ -13,7 +23,8 @@ import {
 } from 'components';
 
 import {
-  Prompt
+  Prompt,
+  IconRenderer
 } from 'helpers';
 
 import Phase from './phase';
@@ -21,14 +32,34 @@ import Phase from './phase';
 
 import styles from './modules.module.sass';
 
+const usePrevious = (value) => {
+  const ref = React.useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 const Modules = ({ _modules, course, setLoaderActive }) => {
+
+
 
   if(!_modules) _modules = [];
 
+  //temp
+  _modules.forEach((obj, index) => {
+    // console.log(obj, index)
+    obj.order = index;
+  })
+
   const [modules, setModules] = useState(_modules);
-  const [modulesRender, setModulesRender] = useState();
+  const [modulesRender, setModulesRender] = useState([]);
   const [phaseOpen, setPhaseOpen] = useState(false);
   const [phaseData, setPhaseData] = useState({});
+
+  // const [currentlyDragging, setCurrentlyDragging] = useState(null);
+
+  let currentlyDragging = null;
 
   //basically if no phase in a module is the one selected. remove any selected phases from that module
   //sounds a bit weird. But each module keeps track of which phase is selected locally
@@ -53,18 +84,6 @@ const Modules = ({ _modules, course, setLoaderActive }) => {
 
   }
 
-  useEffect(() => {
-    setModulesRender(
-      modules.map((obj, index) => {
-
-        // console.log(obj)
-
-        return (
-          <EditableModule removeSelected={(moduleContainSelectedPhase === index) ? false : true} onPhaseClick={(phase) => phaseClick(phase, index)} key={index} name={obj.name} phases={obj.phases} />
-        );
-      }),
-    );
-  }, [modules, phaseOpen, moduleContainSelectedPhase]);
 
   const onModuleCreation = async response => {
     // console.log(response);
@@ -87,17 +106,85 @@ const Modules = ({ _modules, course, setLoaderActive }) => {
   };
 
 
+  // useEffect(() => {
+  //   setModulesRender(
+  //     modules.map((obj, index) => {
+  //
+  //       // console.log(obj)
+  //       // obj.elemId = `${nanoid(6)}_${obj.order}`;
+  //
+  //       // console.log(obj.elemId)
+  //
+  //       //
+  //
+  //       return (
+  //         <EditableModule onPhaseClick={(phase) => phaseClick(phase, index)} elemId={obj.elemId} removeSelected={(moduleContainSelectedPhase === index) ? false : true} key={index} name={obj.name} phases={obj.phases} />
+  //       );
+  //     }),
+  //   );
+  // }, [modules, phaseOpen, moduleContainSelectedPhase]);
+
+
+
+  const [sortableList, setSortableList] = useState([]);
+
+  useEffect(() => {
+
+    setSortableList(modules.map((obj, index) => {
+
+      return {
+        name: obj.name,
+        id: obj.id,
+        phases: obj.phases
+      }
+
+    }))
+
+
+  }, [modules, phaseOpen, moduleContainSelectedPhase])
+
+  let sortableClassName = `${styles.sortable}`;
+
+  const [isAModuleChosen, setIsAModuleChosen] = useState(false);
+
+  useEffect(() => {
+
+    let hasAChosen = false;
+
+    for(let obj of sortableList) {
+      if(obj.chosen) {
+        hasAChosen = true;
+        break;
+      }
+    }
+
+    setIsAModuleChosen(hasAChosen);
+
+  }, [sortableList])
+
   return(
     <div className={(phaseOpen) ? `${styles.page} ${styles.minified}` : styles.page}>
       <div className={styles.wrapper}>
-        {modulesRender?.length === 0 ? (
+        {sortableList?.length === 0 ? (
           <p className={styles.noModulesText}>{lang.noModulesText}</p>
         ) : (
           <>
             <p className={styles.modulesText}>{lang.modules}</p>
 
             <div className={styles.modulesContainer}>
-              {modulesRender}
+              <ReactSortable
+                tag="div"
+                className={sortableClassName}
+                list={sortableList}
+                setList={setSortableList}
+                handle={`.sortable_draggable`}
+                animation={200}
+              >
+                {sortableList.map((obj, index) => {
+                  return (<EditableModule sortableHasOneChosen={isAModuleChosen} chosen={obj.chosen} onPhaseClick={(phase) => phaseClick(phase, index)} removeSelected={(moduleContainSelectedPhase === index) ? false : true} key={obj.id} name={obj.name} phases={obj.phases} />)
+                })}
+              </ReactSortable>
+
             </div>
           </>
         )}
